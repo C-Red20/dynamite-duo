879430663dab02f2fb18aa006b1e0363fb211112
C-Red20 <caila.linger@yahoo.com>
2024-04-16 13:56:26 -0400

    Update Code

---
+++ scripts/database.js
@@ -0,0 +1,34 @@
+const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ],
+    villains: [
+        {
+            id: 2,
+            name: "Ice Man",
+            power: "Freezes things & people"
+        },
+        {
+            id: 2,
+            name: "Slow Poke",
+            power: "Slows things & people down"
+        }
+    ]
+}
+
+export const getHeroes = () => {
+    return database.heroes.map(hero => ({...hero}))
+}
+
+export const getVillains = () => {
+    return database.villains.map(villain => ({...villain}))
+}
